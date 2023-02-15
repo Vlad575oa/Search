5e09e052e9ea9e8d2c77606e5d4e02764385ba9c
Vlad <vlad14196@gmail.com>
2023-02-15 12:17:14 +0300

    Initial Commit

---
+++ ContriesApp/Model.swift
@@ -0,0 +1,14 @@
+//
+//  Model.swift
+//  ContriesApp
+//
+//  Created by user on 15.02.2023.
+//
+
+import Foundation
+
+struct Country {
+    let title: String
+    let imageName: String
+    var isActive: Bool
+}
